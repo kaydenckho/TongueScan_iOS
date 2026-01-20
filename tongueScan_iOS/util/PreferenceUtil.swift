403f90a60f7422140f4b99adf5b6d0d6d3d583e1
kayd3nckh <kaydenckho@gmail.com>
2026-01-20 10:35:16 +0800

    update

---
+++ tongueScan_iOS/util/PreferenceUtil.swift
@@ -0,0 +1,80 @@
+//
+//  PreferenceUtil.swift
+//  tongueScan_iOS
+//
+//  Created by kayd3nckh on 15/12/2023.
+//
+
+import SwiftUI
+
+@Observable
+class PreferenceUtil{
+    
+    let LANGUAGE = "LANGUAGE"
+    let THEME = "THEME"
+    let IS_AGREED_TERMS = "IS_AGREED_TERMS"
+    let IS_REMEMBER_LOGIN = "IS_REMEMBER_LOGIN"
+    let IS_USE_BIOMETRIC_LOGIN = "IS_USE_BIOMETRIC_LOGIN"
+    let USERNAME = "USERNAME"
+    let TOKEN = "TOKEN"
+    
+    init() {
+        language = UserDefaults.standard.string(forKey: LANGUAGE)
+        theme = UserDefaults.standard.object(forKey: THEME) as? ColorScheme
+        isAgreedTerms = UserDefaults.standard.bool(forKey: IS_AGREED_TERMS)
+        isRememberLogin = UserDefaults.standard.bool(forKey: IS_REMEMBER_LOGIN)
+        isUseBiometricLogin = UserDefaults.standard.bool(forKey: IS_USE_BIOMETRIC_LOGIN)
+        username = UserDefaults.standard.string(forKey: USERNAME)
+        token = UserDefaults.standard.string(forKey: TOKEN)
+    }
+
+    
+    var language: String? = nil{
+            didSet {
+                guard oldValue != self.language else { return }
+                UserDefaults.standard.set(self.language, forKey: LANGUAGE)
+            }
+        }
+    
+    var theme: ColorScheme? = nil{
+            didSet {
+                guard oldValue != self.theme else { return }
+                UserDefaults.standard.set(self.theme, forKey: THEME)
+            }
+        }
+    
+    var isAgreedTerms: Bool? = nil{
+            didSet {
+                guard oldValue != self.isAgreedTerms else { return }
+                UserDefaults.standard.set(self.isAgreedTerms, forKey: IS_AGREED_TERMS)
+            }
+        }
+    
+    var isRememberLogin: Bool? = nil{
+            didSet {
+                guard oldValue != self.isRememberLogin else { return }
+                UserDefaults.standard.set(self.isRememberLogin, forKey: IS_REMEMBER_LOGIN)
+            }
+        }
+    
+    var isUseBiometricLogin: Bool? = nil{
+            didSet {
+                guard oldValue != self.isUseBiometricLogin else { return }
+                UserDefaults.standard.set(self.isUseBiometricLogin, forKey: IS_USE_BIOMETRIC_LOGIN)
+            }
+        }
+    
+    var username: String? = nil{
+            didSet {
+                guard oldValue != self.username else { return }
+                UserDefaults.standard.set(self.username, forKey: USERNAME)
+            }
+        }
+    
+    var token: String? = nil{
+            didSet {
+                guard oldValue != self.token else { return }
+                UserDefaults.standard.set(self.token, forKey: TOKEN)
+            }
+        }
+}
